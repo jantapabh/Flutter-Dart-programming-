c898f58f056605d1f44a6343a2f73e393fc6bb1b
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-03 22:40:37 +0700

    add first app

---
+++ firstapp.dart
@@ -0,0 +1,24 @@
+//Write First Flutter App
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPaire = WordPair.random();
+    return MaterialApp(
+      title: "Write First Flutter App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome to Flutter"),
+        ),
+        body: Center(
+          //child: Text("Hello"),
+          child: Text(wordPaire.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
